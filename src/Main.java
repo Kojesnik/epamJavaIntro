23869ae413fcdff78518f29c39536d4a5171607e
Kojesnik <pro100lol55@gmail.com>
2019-07-03 17:25:42 +0300

    Task 3 * ring area * variables fixes * const PI added

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static final double PI = 3.14;
+
     public static void main(String[] args) {
+
+        double a, b, c;                     // Task 1 variables
+        double weight;                      // Task 2 variables
+        double r1, r2;                      // Task 3 variables
 
         /* Task 1
          * difference check between a, b and c
          */
 
-        double a, b, c;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 3 numbers: a, b and c");
         a = scanner.nextDouble();
@@ -20,10 +25,25 @@ public class Main {
          * kg transfer to milligrams, grams and tons
          */
 
-        double weight;
         System.out.println("\nEnter dinosaur weight");
         weight = scanner.nextDouble();
         weightTransfer(weight);
+
+        /* Task 3
+         * ring area
+         */
+
+        System.out.println("\nEnter r1 and r2 circle radius (r1 > r2)");
+        r1 = scanner.nextDouble();
+        r2 = scanner.nextDouble();
+        if(r1 < r2) {
+            while(r1 < r2) {
+                System.out.println("r1 must be > then r2. Enter again");
+                r1 = scanner.nextDouble();
+                r2 = scanner.nextDouble();
+            }
+        }
+        ringArea(r1, r2);
 
     }
 
@@ -52,6 +72,14 @@ public class Main {
         System.out.println(weight + " kg = " + mg + " milligrams");
         System.out.println(weight + " kg = " + g + " grams");
         System.out.println(weight + " kg = " + t + " tons");
+
+    }
+
+    public static void ringArea(double r1, double r2) {
+
+        double S;
+        S = PI * (Math.pow(r1, 2) - Math.pow(r2, 2));
+        System.out.println("Ring area S = " + S);
 
     }
 
